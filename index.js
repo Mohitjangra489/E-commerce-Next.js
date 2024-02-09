0866dd0b251c41014a83d69d8db0c2245b23a084
Mohit <mohitjangra489@gmail.com>
2024-02-09 11:18:30 +0530

    .

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use(cors({
 
 app.use((req, res, next) => {
     // Set CORS headers
-    res.setHeader('Access-Control-Allow-Origin', 'https://optimum-nutrition.vercel.app/');
+    res.setHeader('Access-Control-Allow-Origin', 'https://optimum-nutrition.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,POST,DELETE,PUT');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
