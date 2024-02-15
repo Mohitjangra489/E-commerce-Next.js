8ed8081b9950acb7bce54a31bfbdf8e587cfa88f
Mohit <mohitjangra489@gmail.com>
2024-02-15 23:06:45 +0530

    .

--- src/app/notFound/page.js
+++ src/app/notFound/page.js
@@ -9,7 +9,7 @@ const Notfound = () => {
     <div className='notfound_maindiv'>
         <h4>404</h4>
         <p className='para1_notfound'>page not found</p>
-        <p className='para2_notfound'> We'are sorry, the page you have looked for does not exist in our database!</p>
+        <p className='para2_notfound'>{`We are sorry, the page you have looked for does not exist in our database!`}</p>
         <button onClick={()=>router.push("/")} className='goback_btn'>Go Back To Home Page</button>
     </div>
   )
