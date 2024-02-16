b1c503e8d6164b313adb5c8f9e91e22a68cb7cd6
Mohit <mohitjangra489@gmail.com>
2024-02-16 22:50:00 +0530

    .

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -12,11 +12,19 @@ import SpinnerLoader from '../components/SpinnerLoader';
 import encryptStorage from '../encryptstorage';
 
 const Page = () => {
-
+    const [allAddress, setallAddress] = useState([]);
     const [cartData, setcartData] = useState([]);
     const [subTotal, setsubTotal] = useState(0);
     const [isloading, setisloading] = useState(true);
+    const [selectedAddress, setSelectedAddress] = useState('');
+
+  const handleAddressChange = (event) => {
+  
+    setSelectedAddress(event.target.value);
+  };
     const router = useRouter();
+
+    console.log("user address", allAddress);
 
     const subtotalPrice = () => {
         if (cartData.length) {
@@ -40,14 +48,28 @@ const Page = () => {
         }).catch((error => console.log(error)));
     };
 
+    async function fetchalluseraddress(id) {
+        let userid = id;
+        let req = await api.get(`/alluseraddress?id=${userid}`).then((res) => {
+            const data = res?.data?.addresses;
+            setallAddress(data);
+
+        }).catch((error => console.log(error)));
+    };
+
 
     //.......................................CHECKOUT FUNCTION............................................  
     async function handleCheckout(e) {
         e.preventDefault();
 
-        if (cartData.length && subTotal) {
+       
+          
+        if (cartData.length && subTotal && selectedAddress!='') {
+
+            const shippingAddress=allAddress.find((address)=>address.address_id==selectedAddress);
+            console.log(shippingAddress);
             const securedata = encryptStorage.getItem("encrypted data");
-            const userData=JSON.parse(securedata);
+            const userData = JSON.parse(securedata);
 
             if (!userData) {
                 router.push("/login");
@@ -58,7 +80,9 @@ const Page = () => {
                 const stripe = await loadStripe("pk_test_51Of4JlSHzjhUWugHQqGJDCuj06HRD0AHYBffFFMEG60BKV0BCnaj3tYsbeJfACUjkwD9PmAgKPTfXbXaXH7vLAUA00JGpvaCKW");
 
                 const body = {
-                    products: cartData
+                    userId:userid,
+                    products: cartData,
+                    shippingInfo: shippingAddress
                 }
                 const headers = {
                     "content-Type": "application/json",
@@ -78,6 +102,9 @@ const Page = () => {
                 }
 
             }
+        }else
+        {
+            toast.info("Please select shipping information or address");
         }
 
 
@@ -86,13 +113,14 @@ const Page = () => {
     useEffect(() => {
 
         const securedata = encryptStorage.getItem("encrypted data");
-        const userData=JSON.parse(securedata);
+        const userData = JSON.parse(securedata);
 
         if (!userData) {
             router.push("/login");
         }
         else {
             fetchallcartItems(userData.user_id);
+            fetchalluseraddress(userData.user_id);
         }
 
     }, [])
@@ -104,7 +132,7 @@ const Page = () => {
     return (
         <div className='cartpage_container'>
             <div className='yourcart_div'>
-                <span className='Your_cart'>Your Cart<span className='special_span'>{(!cartData.length != 0 && isloading==false) ?("(Empty)"):""}</span></span>
+                <span className='Your_cart'>Your Cart<span className='special_span'>{(!cartData.length != 0 && isloading == false) ? ("(Empty)") : ""}</span></span>
             </div>
             {
                 isloading ? <SpinnerLoader /> : (
@@ -135,58 +163,95 @@ const Page = () => {
                                 }
 
                             </div>
-                            {
-                                cartData.length != 0 ? (
-                                    <div className="right_div">
-                                        <div className="order_summary">
-                                            <span>Order Summary</span>
-                                        </div>
-                                        <label>Enter Promo Code</label>
-                                        <div className='promo_div'>
-                                            <input type='text' placeholder='Promo Code' style={{ padding: "5px", width: "100%" }} />
-                                            <button className='sub_btn' >Submit</button>
-                                        </div>
-                                        <div style={{ lineHeight: "2.5" }}>
-                                            <h1>Promotions</h1>
-                                            <div className='free_shipping_div'>
-                                                <span>Free Shipping on Orders Above ₹999</span>
-                                                <span>-₹40</span>
-                                            </div>
-                                            <div>
-                                                <ul style={{ paddingLeft: "0px" }}>
-                                                    <li className='order_summary_li'>
-                                                        <span className="key">Subtotal</span>
-                                                        <span className="value">₹{subTotal}</span>
-                                                    </li>
-                                                    <li className='order_summary_li'>
-                                                        <span className="key">Shipping Cost</span>
-                                                        <span className="value">₹40</span>
-                                                    </li>
-                                                    <li className='order_summary_li'>
-                                                        <span className="key">Shipping Discount</span>
-                                                        <span className="value">{subTotal > 999 ? "-₹40" : "₹0"}</span>
-                                                    </li>
-
-                                                    <li className='order_summary_li'>
-                                                        <span className="key">Estimated Total</span>
-                                                        <span className="value">₹{subTotal > 999 ? subTotal : Number(subTotal) + 40}</span>
-                                                    </li>
-                                                </ul>
-                                                <button className='checkout_btn' onClick={handleCheckout}>CHECKOUT</button>
-                                            </div>
-                                        </div>
-
+                            <div className='lower_shipping_div'>
+                                <div className='shipping_info_container'>
+                                    <h1>Shipping Information</h1>
+                                    <div className='shipping_addresses_div'>
+                                        {
+                                            allAddress?.length ?
+                                                allAddress.map((address) => {
+                                                    return (<div className='user_address_div'>
+                                                        <input type='radio' className='radio_input' 
+                                                        value={address?.address_id} id={address?.address_id} 
+                                                        checked={selectedAddress == address?.address_id}
+                                                        onChange={handleAddressChange}
+                                                        />
+                                                        <label htmlFor={address.address_id} >
+                                                            <div>
+                                                                <span>{address?.fullname + " ( " + address?.mobile + " )"}</span><br></br>
+                                                                <span>{address?.address1}</span><br></br>
+                                                                <span>{address?.address2}</span><br></br>
+                                                                <span>{address?.landmark}</span><br></br>
+                                                                <span>{address?.country + "," + address?.city + "," + address?.state + "," + address?.pincode}</span><br></br>
+                                                            </div>
+                                                        </label>
+                                                    </div>)
+                                                })
+                                                : "You have not added any address yet.Please add a new address!"
+                                        }
                                     </div>
-                                ) : ""
-                            }
+                                    <Link href={"/userdashboard/useraddress/addnewaddress"}>
+                                    <button className='addnew_button'>+Add new address</button>
+                                    </Link>
+
+                                </div>
+                                {
+                                    cartData.length != 0 ? (
+                                        <div className="right_div">
+                                            <div className="order_summary">
+                                                <span>Order Summary</span>
+                                            </div>
+                                            <label>Enter Promo Code</label>
+                                            <div className='promo_div'>
+                                                <input type='text' placeholder='Promo Code' style={{ padding: "5px", width: "100%" }} />
+                                                <button className='sub_btn' >Submit</button>
+                                            </div>
+                                            <div style={{ lineHeight: "2.5" }}>
+                                                <h1>Promotions</h1>
+                                                <div className='free_shipping_div'>
+                                                    <span>Free Shipping on Orders Above ₹999</span>
+                                                    <span>-₹40</span>
+                                                </div>
+                                                <div>
+                                                    <ul style={{ paddingLeft: "0px" }}>
+                                                        <li className='order_summary_li'>
+                                                            <span className="key">Subtotal</span>
+                                                            <span className="value">₹{subTotal}</span>
+                                                        </li>
+                                                        <li className='order_summary_li'>
+                                                            <span className="key">Shipping Cost</span>
+                                                            <span className="value">₹40</span>
+                                                        </li>
+                                                        <li className='order_summary_li'>
+                                                            <span className="key">Shipping Discount</span>
+                                                            <span className="value">{subTotal > 999 ? "-₹40" : "₹0"}</span>
+                                                        </li>
+
+                                                        <li className='order_summary_li'>
+                                                            <span className="key">Estimated Total</span>
+                                                            <span className="value">₹{subTotal > 999 ? subTotal : Number(subTotal) + 40}</span>
+                                                        </li>
+                                                    </ul>
+                                                    <button className='checkout_btn' onClick={handleCheckout}>CHECKOUT</button>
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                    ) : ""
+                                }
+
+                            </div>
+
+
 
                         </div>
+
                     </>
                 )}
 
         </div>
     )
-    
+
 }
 
 export default Page
